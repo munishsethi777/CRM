cd40e598621f486c87b5135ec61988fc0e934971
munishsethi777 <munishsethi777@gmail.com>
2015-01-29 11:54:16 +0530

    MVCJqGrid

---
+++ MvcJqGrid/Extensions/ObjectExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Script.Serialization;
+using MvcJqGrid.Utility;
+
+namespace MvcJqGrid.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static string ToJSON(this object obj)
+        {
+            var serializer = new JavaScriptSerializer();
+            serializer.RegisterConverters(new JavaScriptConverter[] { new NullPropertiesConverter() });
+            return serializer.Serialize(obj);
+        }
+
+        public static string ToJSON(this object obj, int recursionDepth)
+        {
+            var serializer = new JavaScriptSerializer();
+            serializer.RegisterConverters(new JavaScriptConverter[] { new NullPropertiesConverter() });
+            serializer.RecursionLimit = recursionDepth;
+            return serializer.Serialize(obj);
+        }
+    }
+}
