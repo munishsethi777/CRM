a569e0d10f9144fbcaabae3e62701d0ec33ea9a4
bgaheer <baljeetgaheer@gmail.com>
2015-02-23 12:10:56 +0530

    IConstants

---
+++ Calendar/IConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCModels
+{
+    public class IConstants
+    {
+        public static string REPEATED = "Repeated";
+        public static string NOT_REPEATED = "Not Repeated";
+    }
+}
